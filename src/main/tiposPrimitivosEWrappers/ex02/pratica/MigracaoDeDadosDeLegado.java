bd14759c517e1b2c70e9336397ce8eeaf9e8095d
rafaeldiasgarcia <rafdgar@gmail.com>
2025-12-31 15:03:04 -0300

    correção no segundo desafio

--- src/main/tiposPrimitivosEWrappers/ex02/pratica/MigracaoDeDadosDeLegado.java
+++ src/main/tiposPrimitivosEWrappers/ex02/pratica/MigracaoDeDadosDeLegado.java
@@ -4,23 +4,26 @@ public class MigracaoDeDadosDeLegado {
 
     public void processarDados(long[] ids, double[] notas) {
 
-        for (int i = 0; i < ids.length; i++) {
+        if (ids.length != notas.length) {
+            System.out.println("Erro: Listas de tamanhos diferentes. Processamento abortado.");
+            return;
+        }
 
+        for (int i = 0; i < ids.length; i++) {
             long idAtual = ids[i];
             double notaAtual = notas[i];
 
-            if (idAtual > Integer.MAX_VALUE) {
+            try {
+
+                int idConvertido = Math.toIntExact(idAtual);
+
+                int notaConvertida = (int) Math.round(notaAtual);
+
+                System.out.println("ID: " + idConvertido + " | Nota: " + notaConvertida);
+
+            } catch (ArithmeticException e) {
                 System.out.println("ID " + idAtual + " ignorado: Overflow");
-                continue;
             }
-
-            int idConvertido = (int) idAtual;
-            int notaConvertida = (int) Math.round(notaAtual);
-
-            System.out.println("ID: " + idConvertido + " | Nota: " + notaConvertida);
-
         }
-
     }
-
 }
