7807dcfb17d5917b1def612c6f5eb5e3d666037d
rafaeldiasgarcia <rafdgar@gmail.com>
2026-01-05 17:13:00 -0300

    refatorando pastas

---
+++ src/test/poo/ex06/TestePersonagem.java
@@ -0,0 +1,13 @@
+package poo.ex06;
+
+import poo.ex06.pratica.Personagem;
+
+public class TestePersonagem {
+    public static void main(String[] args) {
+        Personagem heroi = new Personagem("Heroi", 100);
+        Personagem oponente = new Personagem("Orc", 50);
+
+        System.out.println(heroi);
+        System.out.println(oponente);
+    }
+}
