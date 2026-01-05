2f2244af9e040c78e3879c7e9516e44b20babd48
rafaeldiasgarcia <rafdgar@gmial.com>
2026-01-05 17:13:00 -0300

    refatorando pastas

---
+++ src/main/poo/ex01/pratica/Personagem.java
@@ -0,0 +1,33 @@
+package poo.ex01.pratica;
+
+public class Personagem {
+    private String nome;
+    private int forca;
+
+    public Personagem(String nomePersonagem, int forcaPersonagem) {
+        this.nome = nomePersonagem;
+        this.forca = forcaPersonagem;
+    }
+
+    public void atacar(Personagem oponente) {
+        System.out.println("Força de " + nome + ": " + forca +
+                           "\nForça de " + oponente.nome + ": " + oponente.forca);
+        if (forca > 0 || oponente.forca > 0) {
+            if (forca > oponente.forca) {
+                System.out.println("O " + nome + " deu 10 de dano ao " + oponente.nome + "!");
+                oponente.forca -= 10;
+            } else if (forca < oponente.forca) {
+                System.out.println("O " +oponente.nome + " deu 5 de dano ao " + nome + "!");
+                forca -= 5;
+            }
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+}
---
+++ src/test/poo/ex01/TestePersonagem.java
@@ -0,0 +1,13 @@
+package poo.ex01;
+
+import poo.ex01.pratica.Personagem;
+
+public class TestePersonagem {
+    public static void main(String[] args) {
+        Personagem heroi = new Personagem("Aragorn", 100);
+        Personagem oponente = new Personagem("Orc", 30);
+
+        System.out.println("O " + heroi.getNome() + " tem força " + heroi.getForca());
+        heroi.atacar(oponente);
+    }
+}
