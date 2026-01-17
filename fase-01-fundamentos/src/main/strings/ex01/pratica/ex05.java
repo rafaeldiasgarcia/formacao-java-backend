d9c3d693512054580111e9556bfd5ff8569da3d8
rafaeldiasgarcia <rafdgar@gmail.com>
2026-01-17 17:23:41 -0300

    correção no ex05

--- fase-01-fundamentos/src/main/strings/ex01/pratica/ex05.java
+++ fase-01-fundamentos/src/main/strings/ex01/pratica/ex05.java
@@ -4,7 +4,7 @@ public class ex05 {
     public static void main(String[] args) {
         String texto = "Ame o poema";
         boolean ehPalindromo = verificarPalindromo(texto);
-        System.out.println("\"" + texto + "\" é palíndromo? " + ehPalindromo);
+        System.out.println("\"" + texto + "\" é um palíndromo? " + ehPalindromo);
     }
 
     public static boolean verificarPalindromo(String texto) {
