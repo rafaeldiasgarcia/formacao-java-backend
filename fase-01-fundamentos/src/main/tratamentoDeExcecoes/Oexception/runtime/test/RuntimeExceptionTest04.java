4efb4d81ac79e789bc28951326d3f35a2b55c000
rafaeldiasgarcia <rafdgar@gmail.com>
2026-01-27 16:52:08 -0300

    setimo e oitavo exercicio para a Fase 01 fundamentos: Profissionalismo: Tratamento de Exceções (try-catch, try-with-resources)

--- fase-01-fundamentos/src/main/tratamentoDeExcecoes/Oexception/runtime/test/RuntimeExceptionTest04.java
+++ fase-01-fundamentos/src/main/tratamentoDeExcecoes/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -1,35 +1,54 @@
 package tratamentoDeExcecoes.Oexception.runtime.test;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
+
         try {
 
             throw new RuntimeException();
 
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro do IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro do ArithmeticException");
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
         } catch (RuntimeException e) {
             System.out.println("Dentro do RuntimeException");
-        } // exceções mais genéricas precisam vir no final
+        } // multi catch em linha unsado "|"
+
 
         try {
-
             talvezLanceException();
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (FileNotFoundException e) {
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
-        } // tenho que tratar as duas exceções que podem acontecer
+        }
+
+//        try {
+//
+//            throw new RuntimeException();
+//
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+//        } catch (IndexOutOfBoundsException e) {
+//            System.out.println("Dentro do IndexOutOfBoundsException");
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Dentro do IllegalArgumentException");
+//        } catch (ArithmeticException e) {
+//            System.out.println("Dentro do ArithmeticException");
+//        } catch (RuntimeException e) {
+//            System.out.println("Dentro do RuntimeException");
+//        } // exceções mais genéricas precisam vir no final
+//
+//        try {
+//
+//            talvezLanceException();
+//
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } // tenho que tratar as duas exceções que podem acontecer
 
     }
 
